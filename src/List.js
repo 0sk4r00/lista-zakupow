35f8a5b1caa296b12ceb031a8e4c00abd6bcf767
Oskar <oskarwylucki@gmail.com>
2021-08-13 23:32:38 +0200

    deploy my react app to github pages

---
+++ src/List.js
@@ -0,0 +1,19 @@
+import React from 'react'
+ import { FaEdit, FaTrash } from 'react-icons/fa'
+const List = ({items, removeItem,editItem}) => {
+  return <div className="grocery-list">
+      {items.map((item)=>{
+          const {id,title} = item;
+            return <article key={id} className="grocery-item">
+                <p className="title">{title}
+                    <div className="btn-container">
+                        <button type="button" className="edit-btn" onClick={()=>editItem(id)}><FaEdit/></button>
+                        <button type="button" className="delete-btn" onClick={()=>removeItem(id)}><FaTrash/></button>
+                    </div>
+                </p>
+            </article>
+      })}
+  </div>
+}
+
+export default List
